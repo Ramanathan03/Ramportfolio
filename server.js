ce791e65ad3131529755d041611a181f157218af
Ramanathan03 <Ramanathanannes47@gmail.com>
2020-06-24 00:00:16 +0100

    starting making changes

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 //app.use("/", router);
-app.listen(process.env.PORT || 3002);
+app.listen(process.env.PORT || 5000);
 
 app.use(express.static(path.join(__dirname, 'clientside/build')));
 
