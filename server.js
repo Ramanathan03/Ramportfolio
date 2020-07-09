793e4abc853fb67b687386953622700da6eb1282
Ramanathan03 <Ramanathanannes47@gmail.com>
2020-07-09 15:34:22 +0100

    backend-server running into error

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(cors());
 app.use(compression());
 app.use(express.json());
 app.use("/", router);
-app.listen(process.env.PORT || 3002);
+app.listen(process.env.PORT || 5000);
 
 app.use(express.static(path.join(__dirname, "clientside/build")));
 
